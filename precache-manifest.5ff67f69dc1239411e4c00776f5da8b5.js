3d30d17d69a2ecffad79448146cb5e98f3ccfc5b
Toti <kastriot.kadriu@gmail.com>
2019-03-24 16:03:04 +0100

    Updates

---
+++ precache-manifest.5ff67f69dc1239411e4c00776f5da8b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b62431e73c25fd90ffce53a9c3458d",
+    "url": "/static/media/trianglified.12b62431.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "170eb0152888df1f54d3",
+    "url": "/static/js/main.c41e599c.chunk.js"
+  },
+  {
+    "revision": "56d1d53a0e4e59eeec8e",
+    "url": "/static/js/2.b034af53.chunk.js"
+  },
+  {
+    "revision": "170eb0152888df1f54d3",
+    "url": "/static/css/main.75eda74b.chunk.css"
+  },
+  {
+    "revision": "7a5831ae714f0a215b8b163fc409097b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio-v2/precache-manifest.d2ad0d833e9ccec7fead2825571279da.js"
+  "/precache-manifest.5ff67f69dc1239411e4c00776f5da8b5.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio-v2/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
