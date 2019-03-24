a8382d5f2e18a5e574613e4f442760e0752fcecf
Toti <kastriot.kadriu@gmail.com>
2019-03-24 15:56:31 +0100

    Updates

---
+++ precache-manifest.d2ad0d833e9ccec7fead2825571279da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b62431e73c25fd90ffce53a9c3458d",
+    "url": "/portfolio-v2/static/media/trianglified.12b62431.svg"
+  },
+  {
+    "revision": "ab0ad7f82d55a974fd7a",
+    "url": "/portfolio-v2/static/js/runtime~main.1145c540.js"
+  },
+  {
+    "revision": "f7ec4e3f5b7402e35793",
+    "url": "/portfolio-v2/static/js/main.8639e1bc.chunk.js"
+  },
+  {
+    "revision": "56d1d53a0e4e59eeec8e",
+    "url": "/portfolio-v2/static/js/2.b034af53.chunk.js"
+  },
+  {
+    "revision": "f7ec4e3f5b7402e35793",
+    "url": "/portfolio-v2/static/css/main.77fe9581.chunk.css"
+  },
+  {
+    "revision": "76fddfd1c3a34b69123db45159d7fd7a",
+    "url": "/portfolio-v2/index.html"
+  }
+];
